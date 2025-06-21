ab05c18da5a8ff56bde1b52eb58e8e6ff642ca19
fauxcadet <chouhansourav4@gmail.com>
2025-06-21 08:26:24 +0530

    loge and exp

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -10,6 +10,10 @@ class NetworkSecurityException(Exception):
         self.file_name=exc_tb.tb_frame.f_code.co_filename
     def  __str__(self):
         return "Error Occured in python script name[{0}] line number [{1}] error message [{2}]".format(self.file_name,self.lineno,str(self.error_message))
+
+
+
+
 """if __name__=='__main__':
     try:
         logger.logging.info("Entered the try block ")
@@ -18,3 +22,4 @@ class NetworkSecurityException(Exception):
     except Exception as e:
         raise NetworkSecurityException(e,sys)
         """
+
